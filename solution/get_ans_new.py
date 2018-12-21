5d291238dae05ba85ccfe1ad8057d75c6b7bef45
ciaoSora <657913055@qq.com>
2018-12-21 13:16:01 +0800

    local solution database

--- solution/get_ans_new.py
+++ solution/get_ans_new.py
@@ -1,105 +1,93 @@
 from AutoACMaton import *
 from utility import *
+import sqlite3
 import time
+import random
 
 if not login():
     print("Wrong password or account.")
     print("Please shut the process and correct it in config.json")
     input()
+
+giveup_waiting_time = 30
+ac_waiting_time = 180
+wa_waiting_time = 30
 dic = {}
-last_time = time.time()
-tolerence = 120
+conn = 0
+cursor = 0
 
 def submit_timeout(dic, s):
     global last_time
-    
     res = submit(dic, s)
-    cur_time = time.time()
-    if cur_time - last_time > tolerence:
-        last_time = cur_time
+    last_time += 1
+    if last_time >= tolerence:
+        last_time = 0
+        print("---------------Reach limit------------------")
         return -1
+    if res == 1:
+        q = dic["question"]
+        q = q.replace('"', "'")
+        sen = "insert into answer (question, solution) values (\"" + q + "\", \"" + s + "\")"
+        cursor.execute(sen)
+        conn.commit()
+        print(q + "   " + s)
     return res
 
-def tryTo():
-    try:
-        d = DB(dic["db_id"])
-    except Exception as e:
-        return False
+def getAnswer(dic):
+    s = dic["question"]
+    s = s.replace('"', "'")
+    s = '"' + s + '"'
+    cursor.execute("select solution from answer where question = " + s)
+    values = cursor.fetchall()
+    if len(values) == 0:
+        return "no solution"
+    else:
+        return values[0][0]
 
-    numCol = getColumn(dic)
-    if numCol > 2:
-        return False
-
-    if numCol == 1:
-        # STEP 0: Try numbers
-        mx_row = 0
-        
-        for key in d.tables:
-            mx_row = max(mx_row, len(d.tables[key]))
-
-        res = submit_timeout(dic, "0")
-        if res == -1:
-            return False
-
-        if res == 1:
-            return True
-
-        for i in range(1, mx_row + 1):
-            res = submit_timeout(dic, str(i))
-            if res == 1:
-                return True
-            if res == -1:
-                return False
-            
-        # STEP 1: Try one attribute
-        for table_name in d.tables:
-            tb = d.tables[table_name]
-            for key in tb.keys:
-                if key[1]:    # is foreign key
-                    continue
-                for row in tb.rows:
-                    tem = row[key]
-                    if type(tem) == int and tem <= mx_row:
-                        continue
-                    res = submit_timeout(dic, str(tem))
-                    if res == 1:
-                        return True
-                    if res == -1:
-                        return False
-
-    elif numCol == 2:
-        # STEP 2: Try two attributes
-        for table_name in d.tables:
-            tb = d.tables[table_name]
-            for key1 in tb.keys:
-                if key1[1]:
-                    continue
-                for key2 in tb.keys:
-                    if key2[1] or key1 == key2:
-                        continue
-                    for row in tb.rows:
-                        s = str(row[key1]) + "\\t" + str(row[key2])
-                        res = submit_timeout(dic, s)
-                        if res == 1:
-                            return True
-                        if res == -1:
-                            return False
-            
-    return False
+def randReal(mn, mx):
+    return mn + random.random() * (mx - mn)
 
 def solve():
     global dic
     global last_time
 
     dic = getprob()
-    last_time = time.time()
-    submit(dic, "mdzz\thaha\thehe")
-    return tryTo()
+    ans = getAnswer(dic)
+    if ans == "no solution":
+        print("Ready to giveup")
+        time.sleep(giveup_waiting_time * randReal(0.8, 1.2))
+        return False
+
+    print("Answer found: " + ans)
+    numWA = random.randint(0, 2)
+    res = 0
+    for i in range(numWA):
+        time.sleep(wa_waiting_time * randReal(0.8, 1.2))
+        submit(dic, str(i + 1))
+
+    t = max(0.0, (ac_waiting_time - wa_waiting_time * numWA) * randReal(0.8, 1.2))
+    time.sleep(t)
+    res = submit(dic, ans)
+    
+    return res == 1
 
 num_giveup = 0
 num_success = 0
 num_tot = 0
+conn = sqlite3.connect("ans.db")
+cursor = conn.cursor()
+cursor.execute("select * from sqlite_master where type = 'table' and name = 'answer'")
+values = cursor.fetchall()
+if len(values) == 0:
+    print("ans.db corrupted. Please shut the process")
+    input()
+
 print("AC-Automaton running...")
+
+cursor.execute("select * from answer")
+values = cursor.fetchall()
+print(str(len(values)) + " items found.")
+
 while True:
     num_tot += 1
     if solve():
@@ -115,4 +103,5 @@ while True:
     if num_tot > 100 and num_success / num_tot < 0.1:
         break
 
+print("Over")
 input()
